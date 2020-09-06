0648142bfe24cb8445f018b84e8292c836cce303
joker7t <batoan131196@gmail.com>
2020-09-06 14:09:51 +0700

    init components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { LANDING_PATH } from './asserts/links';
+import Landing from './components/landing/Landing';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<div className="main">
+					<Route exact path={LANDING_PATH} component={Landing} />
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
